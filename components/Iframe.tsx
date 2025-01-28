505a3dff53ed282138241872be5300a7729a1edc
Manuel-latorre <manuel.latorre11@gmail.com>
2025-01-28 17:00:32 -0300

    add font

---
+++ components/Iframe.tsx
@@ -0,0 +1,120 @@
+"use client";
+import React, { useState, useRef, useEffect } from "react";
+import PlayIcon from "./PlayIcon";
+
+const VideoToIframe = () => {
+  const [showVideo, setShowVideo] = useState(true);
+  const [showOverlay, setShowOverlay] = useState(false);
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const iframeRef = useRef<HTMLIFrameElement>(null);
+  const interactionTimeoutRef = useRef<NodeJS.Timeout | null>(null);
+  const [hasInteracted, setHasInteracted] = useState(false);
+
+  const handleStartExperience = () => {
+    setShowVideo(false);
+    setHasInteracted(false);
+  };
+
+  const handleInteraction = () => {
+    setShowOverlay(true);
+  };
+
+  const handleMouseLeave = () => {
+    if (window.matchMedia("(hover: hover)").matches) {
+      setShowOverlay(false);
+    }
+  };
+
+  const handleIframeInteraction = () => {
+    setHasInteracted(true);
+    if (interactionTimeoutRef.current) {
+      clearTimeout(interactionTimeoutRef.current);
+    }
+  };
+
+  useEffect(() => {
+    const video = videoRef.current;
+    if (video && showVideo) {
+      video.play().catch(error => console.log("Auto-play was prevented:", error));
+    }
+  }, [showVideo]);
+
+  useEffect(() => {
+    if (!showVideo) {
+      interactionTimeoutRef.current = setTimeout(() => {
+        if (!hasInteracted) {
+          setShowVideo(true);
+        }
+      }, 10000);
+
+      const iframe = iframeRef.current;
+      const handleIframeInteractionWrapper = () => handleIframeInteraction();
+
+      if (iframe) {
+        iframe.addEventListener('mousemove', handleIframeInteractionWrapper);
+        iframe.addEventListener('touchstart', handleIframeInteractionWrapper);
+      }
+
+      return () => {
+        if (interactionTimeoutRef.current) {
+          clearTimeout(interactionTimeoutRef.current);
+        }
+        if (iframe) {
+          iframe.removeEventListener('mousemove', handleIframeInteractionWrapper);
+          iframe.removeEventListener('touchstart', handleIframeInteractionWrapper);
+        }
+      };
+    }
+  }, [showVideo, hasInteracted]);
+
+  return (
+    <div className="relative w-full h-screen">
+      {showVideo ? (
+        <div 
+          className="relative w-full h-full"
+          onMouseEnter={handleInteraction}
+          onMouseLeave={handleMouseLeave}
+          onTouchStart={handleInteraction}
+        >
+          <video
+            ref={videoRef}
+            className="w-full h-full object-cover"
+            loop 
+            muted
+            playsInline
+          >
+            <source src="https://res.cloudinary.com/drsrva2kp/video/upload/v1737997149/CasaLoft2024_1_1_gegyh5.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+          <div 
+            className={`absolute inset-0 bg-black transition-opacity duration-300 ${
+              showOverlay ? 'opacity-50' : 'opacity-0'
+            }`} 
+          />
+          <button
+            className={`p-2 rounded-full border border-white flex items-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 transition-opacity duration-300 ${
+              showOverlay ? 'opacity-100' : 'opacity-0'
+            }`}
+            onClick={handleStartExperience}
+          >
+            <PlayIcon/> Start Experience
+          </button>
+        </div>
+      ) : (
+        <iframe
+          ref={iframeRef}
+          id="arcane-player-frame"
+          src="https://embed.arcanemirage.com/e782cf6b-32a3-4b2b-a2be-468ec62e4c34?key=aWQ9NTA2NyZrZXk9ZTc4MmNmNmItMzJhMy00YjJiLWEyYmUtNDY4ZWM2MmU0YzM0JnRva2VuPXlSVzUyTDRGaVhicw=="
+          frameBorder="0"
+          width="100%"
+          height="100%"
+          className="w-full h-full"
+          allow="fullscreen; microphone"
+          allowFullScreen
+        />
+      )}
+    </div>
+  );
+};
+
+export default VideoToIframe;
---
+++ components/PlayIcon.tsx
@@ -0,0 +1,18 @@
+import * as React from "react"
+const PlayIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="none"
+    stroke="currentColor"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth={2}
+    className="lucide lucide-play"
+
+  >
+    <path d="m6 3 14 9-14 9V3z" />
+  </svg>
+)
+export default PlayIcon
