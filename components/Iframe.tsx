633bd12a13cf3162f3e685f2cce59c07fa671ae5
Manuel-latorre <manuel.latorre11@gmail.com>
2025-01-29 09:26:33 -0300

    adjust time

--- components/Iframe.tsx
+++ components/Iframe.tsx
@@ -8,7 +8,6 @@ const VideoToIframe = () => {
   const [showOverlay, setShowOverlay] = useState(false);
   const [isDesktop, setIsDesktop] = useState(false);
   const videoRef = useRef<HTMLVideoElement>(null);
-  const iframeRef = useRef<HTMLIFrameElement>(null);
   const inactivityTimerRef = useRef<NodeJS.Timeout | null>(null);
 
   const INACTIVITY_TIMEOUT = 25000; // 25 segundos
@@ -22,99 +21,80 @@ const VideoToIframe = () => {
     resetInactivityTimer();
   };
 
-  const handleInteraction = () => {
-    setShowOverlay(true);
-  };
-
-  const handleMouseLeave = () => {
-    if (isDesktop) {
-      setShowOverlay(false);
-    }
-  };
-
   const resetInactivityTimer = () => {
     if (inactivityTimerRef.current) {
       clearTimeout(inactivityTimerRef.current);
     }
-
     inactivityTimerRef.current = setTimeout(() => {
       setShowVideo(true);
     }, INACTIVITY_TIMEOUT);
   };
 
-  const addEventListeners = () => {
-    const events = ["mousemove", "click", "touchstart", "touchmove", "keydown"];
-    events.forEach(event => document.addEventListener(event, resetInactivityTimer));
-
-    if (iframeRef.current) {
-      events.forEach(event => iframeRef.current?.contentWindow?.document.addEventListener(event, resetInactivityTimer));
-    }
-  };
-
-  const removeEventListeners = () => {
-    const events = ["mousemove", "click", "touchstart", "touchmove", "keydown"];
-    events.forEach(event => document.removeEventListener(event, resetInactivityTimer));
-
-    if (iframeRef.current) {
-      events.forEach(event => iframeRef.current?.contentWindow?.document.removeEventListener(event, resetInactivityTimer));
-    }
-  };
-
   useEffect(() => {
-    if (showVideo) {
-      videoRef.current?.play().catch(error => console.log("Auto-play was prevented:", error));
-    } else {
+    if (!showVideo) {
       resetInactivityTimer();
-      addEventListeners();
-    }
 
-    return () => {
-      removeEventListeners();
-      if (inactivityTimerRef.current) {
-        clearTimeout(inactivityTimerRef.current);
-      }
-    };
+      // Eventos para detectar interacción y reiniciar el temporizador
+      const interactionEvents = ["mousemove", "click", "touchstart", "touchmove", "keydown"];
+
+      const handleUserInteraction = () => {
+        resetInactivityTimer();
+      };
+
+      interactionEvents.forEach(event =>
+        document.addEventListener(event, handleUserInteraction)
+      );
+
+      return () => {
+        if (inactivityTimerRef.current) {
+          clearTimeout(inactivityTimerRef.current);
+        }
+        interactionEvents.forEach(event =>
+          document.removeEventListener(event, handleUserInteraction)
+        );
+      };
+    }
   }, [showVideo]);
 
   return (
     <div className="relative w-full h-screen">
       {showVideo ? (
-        <div 
+        <div
           className="relative w-full h-full"
-          onMouseEnter={handleInteraction}
-          onMouseLeave={handleMouseLeave}
-          onTouchStart={handleInteraction}
+          onMouseEnter={() => setShowOverlay(true)}
+          onMouseLeave={() => isDesktop && setShowOverlay(false)}
+          onTouchStart={() => setShowOverlay(true)}
         >
           <video
             ref={videoRef}
             className="w-full h-full object-cover"
-            loop 
+            loop
             muted
             playsInline
+            autoPlay
           >
             <source src="https://res.cloudinary.com/drsrva2kp/video/upload/v1737997149/CasaLoft2024_1_1_gegyh5.mp4" type="video/mp4" />
-            Your browser does not support the video tag.
+            Tu navegador no soporta el video.
           </video>
-          <div 
+          <div
             className={`absolute inset-0 bg-black transition-opacity duration-300 ${
-              showOverlay ? 'opacity-50' : 'opacity-0'
-            }`} 
+              showOverlay ? "opacity-50" : "opacity-0"
+            }`}
           />
           <button
             className={`px-2 py-1 rounded-full text-center border bg-white text-black flex items-start justify-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 transition-opacity duration-300 ${
-              showOverlay ? 'opacity-100' : 'opacity-0'
+              showOverlay ? "opacity-100" : "opacity-0"
             }`}
             onClick={handleStartExperience}
           >
             <p className="text-xl uppercase tracking-wide translate-y-0.5 translate-x-4 font-medium">Comenzar</p>
             <div className="translate-y-4 translate-x-4">
-              <Arrow/>
+              <Arrow />
             </div>
           </button>
         </div>
       ) : (
         <iframe
-          ref={iframeRef}
           id="arcane-player-frame"
           src="https://embed.arcanemirage.com/e782cf6b-32a3-4b2b-a2be-468ec62e4c34?key=aWQ9NTA2NyZrZXk9ZTc4MmNmNmItMzJhMy00YjJiLWEyYmUtNDY4ZWM2MmU0YzM0JnRva2VuPXlSVzUyTDRGaVhicw=="
           frameBorder="0"
