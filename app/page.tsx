742f62bf7d80d933d048596fe2f6628498362d01
Manuel-latorre <manuel.latorre11@gmail.com>
2025-02-03 15:44:20 -0300

    probando

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import ArcanePlayer from "@/components/ArcanePlayer";
 import VideoToIframe from "@/components/Iframe";
 
 export default function Home() {
   return (
-    <VideoToIframe/>
+    <ArcanePlayer project={{
+      id: 1,
+      key: 'e782cf6b-32a3-4b2b-a2be-468ec62e4c34'
+    }}/>
   );
 }
+
+{/* <VideoToIframe/> */}
